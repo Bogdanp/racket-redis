8ef0e69f5fbe6806099e51dabd067d2923744d3e
Stefan <euler90h@gmail.com>
2015-08-18 23:51:38 -0400

    changed exists to return response instead of bool

--- main.rkt
+++ main.rkt
@@ -55,10 +55,7 @@
 
     (define/public (exists keys)
       (apply-cmd "EXISTS" keys)
-      (let ([r (get-response)])
-        (if (number? r)
-            (= 1 r)
-            #f)))
+      (get-response))
     
     (define/public (set key value)
       (apply-cmd "SET" (list key value))
@@ -129,8 +126,8 @@
 
 (module+ test
   (check-equal? (send redis set "a-number" "1") "OK")
-  (check-true (send redis exists "a-number"))
-  (check-false (send redis exists "some crap"))
+  (check-equal? (send redis exists "a-number") 1)
+  (check-equal? (send redis exists "some crap") 0)
   (check-equal? (send redis get "a-number") "1")
   (check-equal? (send redis incr "a-number") 2)
   (check-equal? (send redis getset "a-number" "4") "2")
@@ -140,7 +137,7 @@
   (check-equal? (send redis set "key1" "fksd") "OK")
   (check-equal? (send redis set "key2" "fdadsf") "OK")
   (check-equal? (send redis set "key3" "bdafg") "OK")
- ; (check-true (send redis exists (list "key1" "key2" "key3")))
+  ;(check-true (send redis exists (list "key1" "key2" "key3")))
   (check-equal? (send redis mget (list "key1" "key2" "key3"))
                 (list "fksd" "fdadsf" "bdafg")))
 
