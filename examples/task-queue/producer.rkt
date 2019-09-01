91915df820994c5c76ece490231ff8c63f99ce34
Bogdan Popa <bogdan@defn.io>
2019-09-01 17:29:36 +0300

    example: show rate at which messages are produced

--- examples/task-queue/producer.rkt
+++ examples/task-queue/producer.rkt
@@ -1,13 +1,26 @@
 #lang racket/base
 
-(require racket/random
+(require racket/format
+         racket/random
          redis)
 
+(define client (make-redis))
 (define message-set
   (list "hello!" "ola!" "howdy!" "wassup?" "servus!"))
 
-(define client (make-redis))
+(define start-time (current-seconds))
+(define produced 0)
+
+(void
+ (thread
+  (lambda _
+    (let loop ()
+      (sleep 1)
+      (define delta (- (current-seconds) start-time))
+      (displayln (format "produced: ~a rate: ~a/s" produced (~r (/ produced delta))))
+      (loop)))))
 
 (let loop ()
   (redis-stream-add! client "messages" "message" (random-ref message-set))
+  (set! produced (add1 produced))
   (loop))
