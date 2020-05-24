e3ae818916f1d0484305e72cd509e63307b8657c
Bogdan Popa <bogdan@defn.io>
2020-05-24 14:17:56 +0300

    tet: grab port from env

--- redis-test/redis/common.rkt
+++ redis-test/redis/common.rkt
@@ -12,11 +12,16 @@
 (define test-host
   (or (getenv "REDIS_HOST") "127.0.0.1"))
 
+(define test-port
+  (string->number (or (getenv "REDIS_PORT") "6379")))
+
 (define test-client
-  (make-redis #:host test-host))
+  (make-redis #:host test-host
+              #:port test-port))
 
 (define test-pool
   (make-redis-pool #:host test-host
+                   #:port test-port
                    #:pool-size 2))
 
 (define-syntax-rule (test-commands message e0 e ...)
