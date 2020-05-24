4f1a7a8b5d7b167ecaea48ba2c8652060ebb8702
Bogdan Popa <bogdan@defn.io>
2020-05-24 14:16:26 +0300

    ci: migrate to setup-racket

--- ci/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-pushd /github/workspace
-raco pkg install --auto --batch redis-lib/ redis-doc/ redis-test/
-popd
--- ci/test.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-pushd /github/workspace
-env REDIS_HOST=redis raco test redis-lib/ redis-test/
-popd
